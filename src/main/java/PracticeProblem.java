16d7e06ec0ce31b770d1250f908fee5b3449cc80
SalmaDanane <343249595@tdsb.ca>
2026-05-29 10:27:38 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,29 @@
-public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+public class PracticeProblem{
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int[] createIntArray() {
+		int[] arr;
+		arr = new int[5];
+		for (int v1 = 0; v1 < arr.length; v1++){
+		    arr[v1]= v1 + 1;
+		}
+		return arr;
 	}
 
+	public static String[] createArray(String v1, String v2, String v3, String v4) {
+		return new String[] {v1,v2,v3,v4};
+	}
+
+	public static String getElement(int i1, String[] arr) {
+		return arr[i1];
+	}
+
+	public static void replaceElement(int i1, double d1, double[] arr) {
+		arr[i1] = d1 ;
+	}
+
+	
 }
